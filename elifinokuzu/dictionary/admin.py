127a415263aab432ca36f1677b321ef1738306c3
sipirsipirmin <tunahandursun@hotmail.com>
2018-07-22 19:53:53 +0300

    Add model: Edge

--- elifinokuzu/dictionary/admin.py
+++ elifinokuzu/dictionary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from dictionary.models import Node
+from dictionary.models import Edge
 
-# Register your models here.
+
+admin.site.register(Node)
+admin.site.register(Edge)
