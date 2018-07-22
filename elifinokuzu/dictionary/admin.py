743ff83f0ea092cbd2f0972d7a6b180a4cec0507
Unknown <huseyin.altunkaynak51@gmail.com>
2018-07-22 19:09:49 +0300

    Add model: Edge

--- elifinokuzu/dictionary/admin.py
+++ elifinokuzu/dictionary/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from dictionary.models import Node
 
-# Register your models here.
+admin.site.register(Node)
--- elifinokuzu/dictionary/models.py
+++ elifinokuzu/dictionary/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-LANGUAGE_CHOICES= (
+LANGUAGE_CHOICES = (
 	("tr", "Turkish"),
 	("fr", "French"),
 	("gr", "German"),
@@ -11,6 +11,13 @@ LANGUAGE_CHOICES= (
 	("en", "English"),
 	("es", "Spanish"),
 )
+
+EDGE_TYPE_CHOICES = (
+	("derives_from", "Derives From"),
+	("symbol_of", "Symbol Of"),
+	("compound_of", "Compound Of"),
+)
+
 class Node(models.Model):
 	"""
 	Node (düğüm)
@@ -20,4 +27,20 @@ class Node(models.Model):
 	language = models.CharField(
 		max_length=255,
 		choices=LANGUAGE_CHOICES
+	)
+
+	def __str__(self):
+		return self.name
+
+
+class Edge(models.Model):
+	"""
+	Holds the relationships between nodes
+	"""
+	source = models.ForeignKey(Node, related_name="incoming")
+	destination = models.ForeignKey(Node, related_name="outgoing")
+	is_directed = models.BooleanField()
+	type_of_edge = models.CharField(
+		max_length=255,
+		choices=EDGE_TYPE_CHOICES
 	)
