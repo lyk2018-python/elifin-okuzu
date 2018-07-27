0b24ca9fd85f9b4ea164cccf10d037b472444487
sipirsipirmin <tunahandursun@hotmail.com>
2018-07-27 11:45:40 +0300

    fix: random function

--- elifinokuzu/accounts/views.py
+++ elifinokuzu/accounts/views.py
@@ -11,7 +11,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('accounts/login')
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
