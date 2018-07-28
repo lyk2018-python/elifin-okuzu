5c6df221faaf61c1ba485bfabd21a7879d86780d
ismaildenizli <schooldaysvp@gmail.com>
2018-07-28 14:22:13 +0300

    Addemail field to registration form

--- elifinokuzu/accounts/views.py
+++ elifinokuzu/accounts/views.py
@@ -1,20 +1,21 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from .forms import UserExtendedCreationForm
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserExtendedCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            email=form.cleaned_data.get('email')
+            user = authenticate(username=username,password=raw_password,email=email)
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        form = UserExtendedCreationForm()
+        return render(request, 'registration/signup.html', {'form': form})
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
