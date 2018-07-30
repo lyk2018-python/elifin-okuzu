b03bcc1a78fbb0ea130764c3fa54b2bbb9c7b228
omerumur14 <umur.omer@hotmail.com>
2018-07-30 09:57:17 +0300

    reports fix

--- elifinokuzu/reports/models.py
+++ elifinokuzu/reports/models.py
@@ -3,6 +3,3 @@ from django.db import models
 class Issue(models.Model):
     title = models.CharField(max_length=255)
     explantation = models.TextField(max_length=2000)
-
-    def __str__(self):
-        return self.url
